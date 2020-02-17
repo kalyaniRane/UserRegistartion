4a3a603c7f976408e745660730eb15666b5ab3ea
kalynaiRane <kalyanirane19@gmail.com>
2020-02-17 18:26:53 +0530

    ADDED : Function To Check The Valid User's EmailID

--- src/main/java/com/bridgelabz/userregistration/UserRegistration.java
+++ src/main/java/com/bridgelabz/userregistration/UserRegistration.java
@@ -2,25 +2,17 @@ package com.bridgelabz.userregistration;
 
 public class UserRegistration {
 
-    private static String FIRST_NAME_PATTERN="^[A-Z][a-z]{3,}$";
-    private static String LAST_NAME_PATTERN="^[A-Z][a-z]{3,}$";
+    private static String VALID_NAME_PATTERN="^[A-Z][a-z]{3,}$";
+    private static String VALID_EMAIL_PATTERN="^([a-zA-Z]{3,}([.|_|+|-]?[a-zA-Z0-9]+)?[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}([.]?[a-zA-Z]{2,3})?)$";
     public static void main(String[] args) {
         System.out.println("Welcome To User Registration");
     }
 
-    public static String checkUserFirstName(String fname) {
-        if(fname.matches(FIRST_NAME_PATTERN)) {
-            return "true";
-        }
-        return "false";
+    public static boolean checkUserName(String name) {
+        return(name.matches(VALID_NAME_PATTERN));
     }
 
-
-    public static String checkUserLastName(String lname) {
-        if(lname.matches(LAST_NAME_PATTERN))
-        {
-            return "true";
-        }
-        return "false";
+    public static boolean checkUserEmail(String email) {
+        return(email.matches(VALID_EMAIL_PATTERN));
     }
 }
--- src/test/java/com/bridgelabz/userregistration/UserValidator.java
+++ src/test/java/com/bridgelabz/userregistration/UserValidator.java
@@ -7,25 +7,37 @@ public class UserValidator {
 
     @Test
     public void givenFirstName_WhenProper_ShouldReturnTrue() {
-        String result=UserRegistration.checkUserFirstName("Della");
-        Assert.assertEquals("true",result);
+        boolean result=UserRegistration.checkUserName("Della");
+        Assert.assertTrue(true);
     }
 
     @Test
-    public void givenFirstName_WhenProper_ShouldReturnFalse() {
-        String result=UserRegistration.checkUserFirstName("della");
-        Assert.assertEquals("false",result);
+    public void givenFirstName_WhenImproper_ShouldReturnFalse() {
+        boolean result=UserRegistration.checkUserName("della");
+        Assert.assertFalse(false);
     }
 
     @Test
     public void givenLastName_WhenProper_ShouldReturnTrue() {
-        String result=UserRegistration.checkUserLastName("Desuza");
-        Assert.assertEquals("true",result);
+        boolean result=UserRegistration.checkUserName("Desuza");
+        Assert.assertTrue(true);
     }
 
     @Test
-    public void givenLastName_WhenProper_ShouldReturnFalse() {
-        String result=UserRegistration.checkUserLastName("desuza");
-        Assert.assertEquals("false",result);
+    public void givenLastName_WhenImproper_ShouldReturnFalse() {
+        boolean result=UserRegistration.checkUserName("desuza");
+        Assert.assertFalse(false);
+    }
+
+    @Test
+    public void givenEmailId_WhenProper_ShouldReturnTrue() {
+        boolean result=UserRegistration.checkUserEmail("abc.xyz@bl.co.in");
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void givenEmailId_WhenImproper_ShouldReturnFalse() {
+        boolean result=UserRegistration.checkUserEmail(".abc@abc.com");
+        Assert.assertFalse(false);
     }
 }
