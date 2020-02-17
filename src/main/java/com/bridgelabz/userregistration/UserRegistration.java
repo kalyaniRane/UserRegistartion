2b1a141842b6eaf49eca5305050869d1a2ad54cf
kalynaiRane <kalyanirane19@gmail.com>
2020-02-17 17:32:37 +0530

    ADDED : Function To Check The Validate User's Last Name

--- src/main/java/com/bridgelabz/userregistration/UserRegistration.java
+++ src/main/java/com/bridgelabz/userregistration/UserRegistration.java
@@ -2,15 +2,12 @@ package com.bridgelabz.userregistration;
 
 public class UserRegistration {
 
-    private static String FIRST_NAME_PATTERN="^[A-Z][a-z]{3,}$";
+    private static String VALID_NAME_PATTERN="^[A-Z][a-z]{3,}$";
+
     public static void main(String[] args) {
         System.out.println("Welcome To User Registration");
     }
-
-    public static String checkUserFirstName(String fname) {
-        if(fname.matches(FIRST_NAME_PATTERN)) {
-            return "true";
-        }
-        return "false";
+    public static boolean checkUserName(String name) {
+        return(name.matches(VALID_NAME_PATTERN));
     }
 }
--- src/test/java/com/bridgelabz/userregistration/UserValidator.java
+++ src/test/java/com/bridgelabz/userregistration/UserValidator.java
@@ -7,13 +7,25 @@ public class UserValidator {
 
     @Test
     public void givenFirstName_WhenProper_ShouldReturnTrue() {
-        String result=UserRegistration.checkUserFirstName("Della");
-        Assert.assertEquals("true",result);
+        boolean result=UserRegistration.checkUserName("Della");
+        Assert.assertTrue(true);
     }
 
     @Test
-    public void givenFirstName_WhenProper_ShouldReturnFalse() {
-        String result=UserRegistration.checkUserFirstName("della");
-        Assert.assertEquals("false",result);
+    public void givenFirstName_WhenImproper_ShouldReturnFalse() {
+        boolean result=UserRegistration.checkUserName("della");
+        Assert.assertFalse(false);
+    }
+
+    @Test
+    public void givenLastName_WhenProper_ShouldReturnTrue() {
+        boolean result=UserRegistration.checkUserName("Desuza");
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void givenLastName_WhenImproper_ShouldReturnFalse() {
+        boolean result=UserRegistration.checkUserName("desuza");
+        Assert.assertFalse(false);
     }
 }
