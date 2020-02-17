212e3857e237d020c7db04eafd47e6e6c18df5dc
kalynaiRane <kalyanirane19@gmail.com>
2020-02-17 17:32:37 +0530

    ADDED : Function To Check The Validate User's Last Name

--- src/main/java/com/bridgelabz/userregistration/UserRegistration.java
+++ src/main/java/com/bridgelabz/userregistration/UserRegistration.java
@@ -3,12 +3,22 @@ package com.bridgelabz.userregistration;
 public class UserRegistration {
 
     private static String FIRST_NAME_PATTERN="^[A-Z][a-z]{3,}$";
+    private static String LAST_NAME_PATTERN="^[A-Z][a-z]{3,}$";
     public static void main(String[] args) {
         System.out.println("Welcome To User Registration");
     }
 
     public static String checkUserFirstName(String fname) {
         if(fname.matches(FIRST_NAME_PATTERN)) {
+            return "true";
+        }
+        return "false";
+    }
+
+
+    public static String checkUserLastName(String lname) {
+        if(lname.matches(LAST_NAME_PATTERN))
+        {
             return "true";
         }
         return "false";
--- src/test/java/com/bridgelabz/userregistration/UserValidator.java
+++ src/test/java/com/bridgelabz/userregistration/UserValidator.java
@@ -16,4 +16,16 @@ public class UserValidator {
         String result=UserRegistration.checkUserFirstName("della");
         Assert.assertEquals("false",result);
     }
+
+    @Test
+    public void givenLastName_WhenProper_ShouldReturnTrue() {
+        String result=UserRegistration.checkUserLastName("Desuza");
+        Assert.assertEquals("true",result);
+    }
+
+    @Test
+    public void givenLastName_WhenProper_ShouldReturnFalse() {
+        String result=UserRegistration.checkUserLastName("desuza");
+        Assert.assertEquals("false",result);
+    }
 }
