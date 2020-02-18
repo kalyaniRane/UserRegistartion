00c1cc188c37b7a60cd62bdd8610ae5a3217bf9a
kalynaiRane <kalyanirane19@gmail.com>
2020-02-18 16:30:26 +0530

    ADDED : Function To Check User's Valid Password At Least 1 UpperCase

--- src/main/java/com/bridgelabz/userregistration/UserRegistration.java
+++ src/main/java/com/bridgelabz/userregistration/UserRegistration.java
@@ -2,13 +2,12 @@ package com.bridgelabz.userregistration;
 
 public class UserRegistration {
 
+
     private static String VALID_NAME_PATTERN="^[A-Z][a-z]{3,}$";
     private static String VALID_EMAIL_PATTERN="^([a-zA-Z]{3,}([.|_|+|-]?[a-zA-Z0-9]+)?[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}([.]?[a-zA-Z]{2,3})?)$";
     private static String VALID_MOBILE_NUMBER="^[1-9]{2}[[' ']][1-9]{1}[0-9]{9}$";
     public static String VALID_PASSWORD_PATTERN1="^[\\w\\W]{8,30}$";
-       public static void main(String[] args) {
-        System.out.println("Welcome To User Registration");
-    }
+    public static String VALID_PASSWORD_PATTERN2 = "^(?=.*[A-Z]).{8,}$";
 
     public static boolean checkUserName(String name) {
         return(name.matches(VALID_NAME_PATTERN));
@@ -24,5 +23,9 @@ public class UserRegistration {
 
     public static boolean checkUserPassword(String password,String pattern) {
         return(password.matches(pattern));
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome To User Registration");
     }
 }
--- src/test/java/com/bridgelabz/userregistration/UserValidator.java
+++ src/test/java/com/bridgelabz/userregistration/UserValidator.java
@@ -8,61 +8,72 @@ public class UserValidator {
     @Test
     public void givenFirstName_WhenProper_ShouldReturnTrue() {
         boolean result=UserRegistration.checkUserName("Della");
-        Assert.assertTrue(true);
+        Assert.assertTrue(result);
     }
 
     @Test
     public void givenFirstName_WhenImproper_ShouldReturnFalse() {
         boolean result=UserRegistration.checkUserName("della");
-        Assert.assertFalse(false);
+        Assert.assertFalse(result);
     }
 
     @Test
     public void givenLastName_WhenImproper_ShouldReturnTrue() {
         boolean result=UserRegistration.checkUserName("Desuza");
-        Assert.assertTrue(true);
+        Assert.assertTrue(result);
     }
 
     @Test
     public void givenLastName_WhenImproper_ShouldReturnFalse() {
         boolean result=UserRegistration.checkUserName("desuza");
-        Assert.assertFalse(false);
+        Assert.assertFalse(result);
     }
 
     @Test
     public void givenEmailId_WhenProper_ShouldReturnTrue() {
         boolean result=UserRegistration.checkUserEmail("abc.xyz@bl.co.in");
-        Assert.assertTrue(true);
+        Assert.assertTrue(result);
     }
 
     @Test
     public void givenEmailId_WhenImproper_ShouldReturnFalse() {
         boolean result=UserRegistration.checkUserEmail(".abc@abc.com");
-        Assert.assertFalse(false);
+        Assert.assertFalse(result);
     }
 
     @Test
     public void givenMobileNumber_WhenProper_ShouldReturnTrue() {
         boolean result=UserRegistration.checkUserMobileNumber("91 9834256734");
-        Assert.assertTrue(true);
+        Assert.assertTrue(result);
     }
 
     @Test
     public void givenMobileNumber_WhenImproper_ShouldReturnFalse() {
         boolean result=UserRegistration.checkUserMobileNumber("919834256734");
-        Assert.assertFalse(false);
+        Assert.assertFalse(result);
     }
 
     @Test
     public void givenPassword_WhenMinimum8Character_ShouldReturnTrue() {
         boolean result=UserRegistration.checkUserPassword("anbc@123",UserRegistration.VALID_PASSWORD_PATTERN1);
-        Assert.assertTrue(true);
+        Assert.assertTrue(result);
     }
 
     @Test
     public void givenPassword_WhenLessThan8Character_ShouldReturnFalse() {
         boolean result=UserRegistration.checkUserPassword("abc@123",UserRegistration.VALID_PASSWORD_PATTERN1);
-        Assert.assertFalse(false);
+        Assert.assertFalse(result);
     }
 
+    @Test
+    public void givenPassword_WhenAtLeast1UpperCase_ShouldReturnTrue() {
+        boolean result=UserRegistration.checkUserPassword("Anbc@123",UserRegistration.VALID_PASSWORD_PATTERN2);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void givenPassword_WhenAtLeast1UpperCase_ShouldReturnFalse() {
+        boolean result=UserRegistration.checkUserPassword("abcf@123",UserRegistration.VALID_PASSWORD_PATTERN2);
+        Assert.assertFalse(result);
+    }
 }
