b01acee50e84e7d1e76abaf68209743f315b35f9
kalynaiRane <kalyanirane19@gmail.com>
2020-02-18 10:33:09 +0530

    ADDED : Function To Check Valid User's Mobile Number

--- src/main/java/com/bridgelabz/userregistration/UserRegistration.java
+++ src/main/java/com/bridgelabz/userregistration/UserRegistration.java
@@ -4,7 +4,9 @@ public class UserRegistration {
 
     private static String VALID_NAME_PATTERN="^[A-Z][a-z]{3,}$";
     private static String VALID_EMAIL_PATTERN="^([a-zA-Z]{3,}([.|_|+|-]?[a-zA-Z0-9]+)?[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}([.]?[a-zA-Z]{2,3})?)$";
-    public static void main(String[] args) {
+    private static String VALID_MOBILE_NUMBER="^[1-9]{2}[[' ']][1-9]{1}[0-9]{9}$";
+    
+       public static void main(String[] args) {
         System.out.println("Welcome To User Registration");
     }
 
@@ -17,6 +19,14 @@ public class UserRegistration {
 
     public static String checkUserEmail(String email) {
         if(email.matches(VALID_EMAIL_PATTERN))
+        {
+            return "true";
+        }
+        return "false";
+    }
+
+    public static String checkUserMobileNumber(String mobNumber) {
+        if(mobNumber.matches(VALID_MOBILE_NUMBER))
         {
             return "true";
         }
--- src/test/java/com/bridgelabz/userregistration/UserValidator.java
+++ src/test/java/com/bridgelabz/userregistration/UserValidator.java
@@ -12,19 +12,19 @@ public class UserValidator {
     }
 
     @Test
-    public void givenFirstName_WhenProper_ShouldReturnFalse() {
+    public void givenFirstName_WhenImproper_ShouldReturnFalse() {
         String result=UserRegistration.checkUserName("della");
         Assert.assertEquals("false",result);
     }
 
     @Test
-    public void givenLastName_WhenProper_ShouldReturnTrue() {
+    public void givenLastName_WhenImproper_ShouldReturnTrue() {
         String result=UserRegistration.checkUserName("Desuza");
         Assert.assertEquals("true",result);
     }
 
     @Test
-    public void givenLastName_WhenProper_ShouldReturnFalse() {
+    public void givenLastName_WhenImproper_ShouldReturnFalse() {
         String result=UserRegistration.checkUserName("desuza");
         Assert.assertEquals("false",result);
     }
@@ -36,8 +36,20 @@ public class UserValidator {
     }
 
     @Test
-    public void givenEmailId_WhenProper_ShouldReturnFalse() {
+    public void givenEmailId_WhenImproper_ShouldReturnFalse() {
         String result=UserRegistration.checkUserEmail(".abc@abc.com");
+        Assert.assertEquals("false",result);
+    }
+
+    @Test
+    public void givenMobileNumber_WhenProper_ShouldReturnTrue() {
+        String result=UserRegistration.checkUserMobileNumber("91 9834256734");
+        Assert.assertEquals("true",result);
+    }
+
+    @Test
+    public void givenMobileNumber_WhenImproper_ShouldReturnFalse() {
+        String result=UserRegistration.checkUserMobileNumber("919834256734");
         Assert.assertEquals("false",result);
     }
 }
