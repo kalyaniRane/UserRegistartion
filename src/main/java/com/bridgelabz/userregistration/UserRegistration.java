80858c1570efc82ff50c8835dee44ea3b722f5c8
kalynaiRane <kalyanirane19@gmail.com>
2020-02-18 12:33:03 +0530

    ADDED : Function To Check User's Valid Password Length Min 8Characters

--- src/main/java/com/bridgelabz/userregistration/UserRegistration.java
+++ src/main/java/com/bridgelabz/userregistration/UserRegistration.java
@@ -5,31 +5,24 @@ public class UserRegistration {
     private static String VALID_NAME_PATTERN="^[A-Z][a-z]{3,}$";
     private static String VALID_EMAIL_PATTERN="^([a-zA-Z]{3,}([.|_|+|-]?[a-zA-Z0-9]+)?[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}([.]?[a-zA-Z]{2,3})?)$";
     private static String VALID_MOBILE_NUMBER="^[1-9]{2}[[' ']][1-9]{1}[0-9]{9}$";
-    
+    public static String VALID_PASSWORD_PATTERN1="^[\\w\\W]{8,30}$";
        public static void main(String[] args) {
         System.out.println("Welcome To User Registration");
     }
 
-    public static String checkUserName(String name) {
-        if(name.matches(VALID_NAME_PATTERN)) {
-            return "true";
-        }
-        return "false";
+    public static boolean checkUserName(String name) {
+        return(name.matches(VALID_NAME_PATTERN));
     }
 
-    public static String checkUserEmail(String email) {
-        if(email.matches(VALID_EMAIL_PATTERN))
-        {
-            return "true";
-        }
-        return "false";
+    public static boolean checkUserEmail(String email) {
+        return(email.matches(VALID_EMAIL_PATTERN));
     }
 
-    public static String checkUserMobileNumber(String mobNumber) {
-        if(mobNumber.matches(VALID_MOBILE_NUMBER))
-        {
-            return "true";
-        }
-        return "false";
+    public static boolean checkUserMobileNumber(String mobNumber) {
+        return(mobNumber.matches(VALID_MOBILE_NUMBER));
+    }
+
+    public static boolean checkUserPassword(String password,String pattern) {
+        return(password.matches(pattern));
     }
 }
--- src/test/java/com/bridgelabz/userregistration/UserValidator.java
+++ src/test/java/com/bridgelabz/userregistration/UserValidator.java
@@ -7,49 +7,62 @@ public class UserValidator {
 
     @Test
     public void givenFirstName_WhenProper_ShouldReturnTrue() {
-        String result=UserRegistration.checkUserName("Della");
-        Assert.assertEquals("true",result);
+        boolean result=UserRegistration.checkUserName("Della");
+        Assert.assertTrue(true);
     }
 
     @Test
     public void givenFirstName_WhenImproper_ShouldReturnFalse() {
-        String result=UserRegistration.checkUserName("della");
-        Assert.assertEquals("false",result);
+        boolean result=UserRegistration.checkUserName("della");
+        Assert.assertFalse(false);
     }
 
     @Test
     public void givenLastName_WhenImproper_ShouldReturnTrue() {
-        String result=UserRegistration.checkUserName("Desuza");
-        Assert.assertEquals("true",result);
+        boolean result=UserRegistration.checkUserName("Desuza");
+        Assert.assertTrue(true);
     }
 
     @Test
     public void givenLastName_WhenImproper_ShouldReturnFalse() {
-        String result=UserRegistration.checkUserName("desuza");
-        Assert.assertEquals("false",result);
+        boolean result=UserRegistration.checkUserName("desuza");
+        Assert.assertFalse(false);
     }
 
     @Test
     public void givenEmailId_WhenProper_ShouldReturnTrue() {
-        String result=UserRegistration.checkUserEmail("abc.xyz@bl.co.in");
-        Assert.assertEquals("true",result);
+        boolean result=UserRegistration.checkUserEmail("abc.xyz@bl.co.in");
+        Assert.assertTrue(true);
     }
 
     @Test
     public void givenEmailId_WhenImproper_ShouldReturnFalse() {
-        String result=UserRegistration.checkUserEmail(".abc@abc.com");
-        Assert.assertEquals("false",result);
+        boolean result=UserRegistration.checkUserEmail(".abc@abc.com");
+        Assert.assertFalse(false);
     }
 
     @Test
     public void givenMobileNumber_WhenProper_ShouldReturnTrue() {
-        String result=UserRegistration.checkUserMobileNumber("91 9834256734");
-        Assert.assertEquals("true",result);
+        boolean result=UserRegistration.checkUserMobileNumber("91 9834256734");
+        Assert.assertTrue(true);
     }
 
     @Test
     public void givenMobileNumber_WhenImproper_ShouldReturnFalse() {
-        String result=UserRegistration.checkUserMobileNumber("919834256734");
-        Assert.assertEquals("false",result);
+        boolean result=UserRegistration.checkUserMobileNumber("919834256734");
+        Assert.assertFalse(false);
     }
+
+    @Test
+    public void givenPassword_WhenMinimum8Character_ShouldReturnTrue() {
+        boolean result=UserRegistration.checkUserPassword("anbc@123",UserRegistration.VALID_PASSWORD_PATTERN1);
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void givenPassword_WhenLessThan8Character_ShouldReturnFalse() {
+        boolean result=UserRegistration.checkUserPassword("abc@123",UserRegistration.VALID_PASSWORD_PATTERN1);
+        Assert.assertFalse(false);
+    }
+
 }
